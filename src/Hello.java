8c80ac5db4da034482077e3c638d5b95eb170823
Kerime Unal <kerime.cakalli@gmail.com>
2022-12-09 09:19:22 +0300

    updated more ello feature

--- src/Hello.java
+++ src/Hello.java
@@ -3,5 +3,7 @@ public class Hello {
 
         System.out.println("work1 started");
         System.out.println("created git reporisotry");
+        System.out.println("girhou repo is publshied to github");
+        System.out.println("doing more work");
     }
 }
